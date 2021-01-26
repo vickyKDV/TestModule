4a354e0a6f56139b0bbf495c3523d521b8d9f1c1
VickyKDV <vickykdv@gmail.com>
2021-01-26 23:57:14 +0700

    Initial commit

---
+++ app/src/main/java/kreigan/id/testmodule/MainActivity.kt
@@ -0,0 +1,11 @@
+package kreigan.id.testmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
